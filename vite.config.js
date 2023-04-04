97ca7a25006bd519b8549f76ccbd7a6ef9142e3f
Hugo Leal <hugolardosa@gmail.com>
2023-04-04 20:37:54 +0100

    tailored for githubpages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/makenrace_frontend/', // for github pages
   define: {
     'process.env': process.env
   },
